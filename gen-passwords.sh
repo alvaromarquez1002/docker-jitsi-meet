925456eac213b8aa72d01c1b775e7e90138f85c5
amq1002 <amq1002@alu.ubu.es>
2025-05-12 17:38:23 +0200

    changes

--- gen-passwords.sh
+++ gen-passwords.sh
@@ -11,6 +11,17 @@ JIBRI_RECORDER_PASSWORD=$(generatePassword)
 JIBRI_XMPP_PASSWORD=$(generatePassword)
 JIGASI_TRANSCRIBER_PASSWORD=$(generatePassword)
 
+# ---- NUEVAS LÍNEAS PARA MOSTRAR LAS CONTRASEÑAS ----
+echo "Por favor, copia estas contraseñas en tu archivo .env si no se actualizan automáticamente:"
+echo "JICOFO_AUTH_PASSWORD=${JICOFO_AUTH_PASSWORD}"
+echo "JVB_AUTH_PASSWORD=${JVB_AUTH_PASSWORD}"
+echo "JIGASI_XMPP_PASSWORD=${JIGASI_XMPP_PASSWORD}"
+echo "JIBRI_RECORDER_PASSWORD=${JIBRI_RECORDER_PASSWORD}"
+echo "JIBRI_XMPP_PASSWORD=${JIBRI_XMPP_PASSWORD}"
+echo "JIGASI_TRANSCRIBER_PASSWORD=${JIGASI_TRANSCRIBER_PASSWORD}"
+echo "----------------------------------------------------"
+# ----------------------------------------------------
+
 sed -i.bak \
     -e "s#JICOFO_AUTH_PASSWORD=.*#JICOFO_AUTH_PASSWORD=${JICOFO_AUTH_PASSWORD}#g" \
     -e "s#JVB_AUTH_PASSWORD=.*#JVB_AUTH_PASSWORD=${JVB_AUTH_PASSWORD}#g" \
